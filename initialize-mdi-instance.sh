06ec446fa69206c81e8cc1f368b00a7bf9635cf3
Thomas E Wilson <62477613+wilsonte-umich@users.noreply.github.com>
2026-06-11 14:27:09 -0400

    Add -y flag to apt-get install commands

--- initialize-mdi-instance.sh
+++ initialize-mdi-instance.sh
@@ -32,7 +32,7 @@ sudo apt-get install -y \
 # install Docker, now including docker-compose via plugin
 echo 
 echo "install Docker engine"
-sudo apt-get install \
+sudo apt-get install -y \
   ca-certificates \
   curl \
   gnupg \
@@ -43,7 +43,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # allow user ubuntu to control docker without sudo
 echo 
